dbdc76c67c0532e2f88e9bde39fd27f280626078
Katiana Hanisch <katianahanisch@gmail.com>
2024-02-18 07:48:46 -0400

    feat: creating header

---
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function useLocalStorage<T>(item: string) {
+  const storedValue = localStorage.getItem(item);
+  const initialValue = storedValue ? JSON.parse(storedValue) : "";
+  const [value, setValue] = useState<T>(initialValue);
+
+  console.log(storedValue);
+
+  const updateLocalStorage = (newValue: T) => {
+    setValue(newValue);
+    localStorage.setItem(item, JSON.stringify(newValue));
+  };
+
+  return {
+    value,
+    updateLocalStorage,
+  };
+}
