b68406fe020b48dbb078456b9d50162052526656
Katiana Hanisch <katianahanisch@gmail.com>
2024-02-20 17:12:58 -0400

    feat: add filter by search

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { ProductsFetcherResponse } from "@/types/products-response";
 import { useFilter } from "./useFilter";
 import { mountQuery } from "@/utils/graphql-filters";
+import { useDeferredValue } from "react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
@@ -14,14 +15,21 @@ const fetcher = (query: string): AxiosPromise<ProductsFetcherResponse> => {
 };
 
 export function useProducts() {
-  const { type, priority } = useFilter();
+  const { type, priority, search } = useFilter();
+  const searchDeferred = useDeferredValue(search);
   const query = mountQuery(type, priority);
   const { data } = useQuery({
     queryFn: () => fetcher(query),
     queryKey: ["products", type, priority],
   });
 
+  const products = data?.data?.data?.allProducts;
+
+  const filteredProducts = products?.filter((product) =>
+    product.name.toLowerCase().includes(searchDeferred.toLowerCase())
+  );
+
   return {
-    data: data?.data?.data?.allProducts,
+    data: filteredProducts,
   };
 }
