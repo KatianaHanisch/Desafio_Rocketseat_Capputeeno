8b715fd03a0107eeb16088259f0fdb1c70ebff38
Katiana Hanisch <katianahanisch@gmail.com>
2024-02-22 11:42:46 -0400

    refactor: extract providers

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,21 +1,13 @@
-"use client";
-
 import styles from "./page.module.css";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { ProductsList } from "@/components/products-list";
 import { FilterBar } from "@/components/filter-bar";
 
-const queryClient = new QueryClient();
-
 export default function Home() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className={styles.main}>
-        <FilterBar />
-        <ProductsList />
-      </main>
-    </QueryClientProvider>
+    <main className={styles.main}>
+      <FilterBar />
+      <ProductsList />
+    </main>
   );
 }
